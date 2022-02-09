014e87ef9a0a55ee45501ff5f2b8eb93a0894332
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2022-02-09 19:46:58 +0300

    dotnetcoreproject
    .Net Core Camp ( Murat Yücedağ )

---
+++ Controllers/category.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COREDEMO.Controllers
+{
+    public class category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
